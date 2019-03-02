bb53052e06e17362987437ec227bbe8f8afbf47c
Arijit <arijit.sur1998@gmail.com>
2019-03-02 15:07:41 +0530

    Second Commit

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/home.html', {})
+    return render(request, 'blog/sample/homapge.html', {})
