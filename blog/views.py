e727f7bddf6ee281cc198dd5ec771a328ffd87c7
Arijit <arijit.sur1998@gmail.com>
2019-03-03 13:06:42 +0530

    Seven  Commit

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/sample/index.html', {})
+    return render(request, 'blog/index.html', {})
 
 def fun(request):
-    return render(request, 'blog/sample/homapge.html',)
+    return render(request, 'blog/homapge.html',)
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('homapge.html', include('blog.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
